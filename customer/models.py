99a2f1722fbd1970338051a913bbc041e8a839df
Adson Rodrigues <adson.develop@gmail.com>
2020-12-10 01:30:27 -0300

    feat: adição de aplicativo de locatário.

---
+++ customer/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django_tenants.models import TenantMixin, DomainMixin
+
+
+class Client(TenantMixin):
+    name = models.CharField(max_length=100)
+
+
+class Domain(DomainMixin):
+    pass
